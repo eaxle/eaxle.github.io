5ca38cfddf4a25186b68b90bca448c079030abe8
ramesh <ramesh.khadka@zurelsoft.com>
2015-11-27 17:31:21 +0545

    Revert "modified annotation"
    This reverts commit e7bd9384130aaf8247b030bec6d818c8e062ee81.

--- web/showData.js
+++ web/showData.js
@@ -33,22 +33,10 @@ function getHightlightCoords() {
 }
 //highlights the selected div with given color
 function showHighlight(selected) {
-	var container=null;
     var pageIndex = selected.page;
     var page = window.PDFViewerApplication.pdfViewer._pages[pageIndex];
-	var pageElement = page.canvas.parentElement;
-	if($("#pageContainer"+(pageIndex+1)).children(".customAnnotation").length==0){
-	//var pageElement = page.canvas.parentNode.nextSibling;
-	container=document.createElement('div');//this contains all the highlights.
-	container.setAttribute('style',pageElement.getAttribute('style'))
-	container.setAttribute('class','customAnnotation')
-	
-	document.getElementById('pageContainer'+(pageIndex+1)).appendChild(container)
-}
-else{
-	container=$("#pageContainer"+(pageIndex+1)).children(".customAnnotation")[0];
-}
-	//pageElement.appendChild(container);
+	//var pageElement = page.canvas.parentElement;
+	var pageElement = page.canvas.parentNode.nextSibling;
     var viewport = page.viewport;
     var color=document.getElementById('colorPicker').value;
     selected.coords.forEach(function (rect) {
@@ -57,17 +45,8 @@ else{
         el.setAttribute('style', 'position: absolute; border:none; background-color:'+color+';' +
             'left:' + Math.min(bounds[0], bounds[2]) + 'px; top:' + Math.min(bounds[1], bounds[3]) + 'px;' +
             'width:' + Math.abs(bounds[0] - bounds[2]) + 'px; height:' + Math.abs(bounds[1] - bounds[3]) + 'px;z-index:90;');
-        container.appendChild(el);
-        //pageElement.appendChild(el);
+        pageElement.appendChild(el);
       
     })
 
 }
-/*
-var event = document.createEvent('CustomEvent');
-      event.initCustomEvent('pagerendered', true, true, {
-        pageNumber: 1,
-        cssTransform: true
-      });
-      document.dispatchEvent(event);
-	  */
